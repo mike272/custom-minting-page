74e62927511e70b1b4f878bc60036bdd9344a4fb
Michal Babinski <babinmichal@gmail.com>
2024-06-12 18:34:19 +0200

    [FEATURE] Implement custom wallet buttons

--- src/lib/components/connect-side-bar.tsx
+++ src/lib/components/connect-side-bar.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { WalletButton } from "@rainbow-me/rainbowkit";
+import { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 
 export function ConnectSideBar({
@@ -8,11 +10,42 @@ export function ConnectSideBar({
   isOpen: boolean;
   setOpen: (open: boolean) => void;
 }) {
+  function CustomWalletButton({
+    ready,
+    connect,
+    connector,
+  }: {
+    ready: any;
+    connect: any;
+    connector: any;
+  }) {
+    const [icon, setIcon] = useState(null);
+
+    useEffect(() => {
+      const fetchIconUrl = async () => {
+        const url = await connector?.iconUrl();
+        setIcon(url);
+      };
+
+      fetchIconUrl();
+    }, [connector]);
+    return (
+      <div className="wallet-button">
+        <img className="wallet-image" src={icon ?? ""} alt={connector?.name} />
+        <button type="button" disabled={!ready} onClick={connect}>
+          {connector?.name}
+        </button>
+      </div>
+    );
+  }
+
+  const walletsList = ["rainbow", "metamask", "coinbase", "phantom"];
+
   return (
     <div
       className="sidebar"
       style={{
-        display: isOpen ? "block" : "none",
+        display: isOpen ? "flex" : "none",
       }}
     >
       <div className="sidebar-top">
@@ -22,9 +55,30 @@ export function ConnectSideBar({
           <IoClose size={24} />
         </div>
       </div>
-      <WalletButton wallet="rainbow" />
-      <WalletButton wallet="metamask" />
-      <WalletButton wallet="coinbase" />
+      {walletsList.map((wallet) => (
+        <WalletButton.Custom key={wallet} wallet={wallet}>
+          {({
+            ready,
+            connect,
+            connector,
+          }: {
+            ready: any;
+            connect: any;
+            connector: any;
+          }) => {
+            console.log({ connector });
+            return (
+              <CustomWalletButton
+                ready={ready}
+                connect={connect}
+                connector={connector}
+                key={connector?.name}
+              />
+            );
+          }}
+        </WalletButton.Custom>
+      ))}
+
       <div className="sidebar-bottom">
         <div>{"Don't have a wallet? "}</div>
         <div className="learn-more-text">Learn more</div>
